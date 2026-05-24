8add9f2af10b0602692d1207d5722ccdaff87316
kxzto <kazutoxmano@gmail.com>
2026-05-24 22:44:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,17 @@
+import socket
 import time
+import requests
 
 while True:
-    print("alive")
-    time.sleep(30)
+    try:
+        ip = socket.gethostbyname("google.com")
+        print(f"DNS OK -> {ip}")
+
+        r = requests.get("https://google.com", timeout=10)
+
+        print(f"Google status -> {r.status_code}")
+
+    except Exception as e:
+        print("ERROR:", e)
+
+    time.sleep(60)

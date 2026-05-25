b6bf8892b85c957b0db19a93c28ac3af88706a3a
kxzto <kazutoxmano@gmail.com>
2026-05-26 00:15:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,30 @@
 import socket
 import time
 import requests
+from datetime import datetime
 
 while True:
     try:
         ip = socket.gethostbyname("google.com")
-        print(f"DNS OK -> {ip}")
 
         r = requests.get("https://google.com", timeout=10)
 
-        print(f"Google status -> {r.status_code}")
+        msg = f"""
+TIME: {datetime.now()}
+DNS: OK -> {ip}
+GOOGLE: {r.status_code}
+"""
+
+        print(msg)
+
+        with open("status.txt", "w") as f:
+            f.write(msg)
 
     except Exception as e:
-        print("ERROR:", e)
+        err = f"ERROR: {e}"
+        print(err)
+
+        with open("status.txt", "w") as f:
+            f.write(err)
 
     time.sleep(60)
